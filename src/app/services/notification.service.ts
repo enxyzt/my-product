d0cc405ee9dcefff730ca8c19b73452ebb10723b
Maxim <llungu.maxim@gmail.com>
2022-05-16 11:01:59 +0300

    created local api server

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -15,45 +15,16 @@ export class NotificationService {
     public dialog: MatDialog
   ) { }
 
-  /**
-   * Presents a toast displaying the message with a green background
-   * @param message Message to display
-   * @example
-   * this.notificationService.success("confirm oked");
-   */
+
   success(message: string) {
     this.openSnackBar(message, '', 'success-snackbar');
   }
 
-  /**
-   * Presents a toast displaying the message with a red background
-   * @param message Message to display
-   * @example
-   * this.notificationService.error("confirm canceled");
-   */
+
   error(message: string) {
     this.openSnackBar(message, '', 'error-snackbar');
   }
 
-  /**
-   * Shows a confirmation modal, presenting the user with
-   * an OK and Cancel button.
-   * @param message Body of the modal
-   * @param okCallback Optional function to call when the user clicks Ok
-   * @param title Optional modal title
-   * @param cancelCallback Option function to call when the user clicks Cancel
-   * @example
-   * //displays a success or error message depending on what button is clicked.
-   * this.notificationService.confirmation(
-   * 'it will be gone forever', //message body
-   * () => { //okCallback
-      this.notificationService.success("confirm oked");
-    },
-    'Are you sure?', //title
-     () => { //cancelCallback
-      this.notificationService.error("confirm canceled");
-    });
-   */
   confirmation(
     message: string,
     okCallback: () => void,
@@ -75,17 +46,7 @@ export class NotificationService {
     });
   }
 
-  /**
-  * Shows a modal, presenting the user with an OK button.
-  * @param message Body of the modal
-  * @param okCallback Optional function to call when the user clicks Ok
-  * @param title Optional modal title
-  * @example
-  * //displays a success when the Ok button is clicked.
-  *  this.notificationService.alert("an alert", "notice", () => {
-      this.notificationService.success("alert oked");
-    });
-  */
+
   alert(message: string, title = 'Notice', okCallback: () => void = () => { }) {
     const dialogRef = this.dialog.open(AlertDialog, {
       width: '250px',
@@ -101,13 +62,7 @@ export class NotificationService {
   }
 
 
-  /**
-   * Displays a toast with provided message
-   * @param message Message to display
-   * @param action Action text, e.g. Close, Done, etc
-   * @param className Optional extra css class to apply
-   * @param duration Optional number of SECONDS to display the notification for
-   */
+
   openSnackBar(
     message: string,
     action: string,
